be28967d98cdd00ffacd7b7679fbc30dd2980807
arty gvozdenkov <gvozdenkov@gmail.com>
2024-06-25 12:22:49 +0300

    ci(back): add test crash route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -7,6 +7,11 @@ import { createUser, login } from '../controllers/users';
 import { validateUserBody, validateAuthentication } from '../middlewares/validatons';
 
 const router = Router();
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 router.post('/signup', validateUserBody, createUser);
 router.post('/signin', validateAuthentication, login);
 
