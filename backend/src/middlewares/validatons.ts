162357a411c2ae176623e997ea3f1c66299dcf4f
arty gvozdenkov <gvozdenkov@gmail.com>
2024-06-26 10:04:07 +0300

    fix(front): url regex

--- backend/src/middlewares/validatons.ts
+++ backend/src/middlewares/validatons.ts
@@ -2,7 +2,7 @@ import { Joi, celebrate } from 'celebrate';
 import { Types } from 'mongoose';
 
 // eslint-disable-next-line no-useless-escape
-const urlRegExp = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:\/?#[\]@!$&'()*+,;=.]+$/;
+const urlRegExp = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 const validateObjId = celebrate({
   params: Joi.object().keys({
